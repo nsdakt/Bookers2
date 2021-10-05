e97fb9a160ad91a0bee421528b548191b84b9cf8
nsdakt <dnj.0077@gmail.com>
2021-10-05 13:49:50 +0000

    [add]searches_form

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create, :destroy]
   end
 
+  get '/search', to: 'searches#search'
+
    #get 'users/:id' => 'users#show', as: :mypage
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  end
