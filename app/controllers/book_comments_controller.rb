4457728fb86d9cb46378c113bce293c2f95da697
nsdakt <dnj.0077@gmail.com>
2021-10-07 18:01:00 +0000

    [fix]comnent_ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,6 +1,5 @@
 class BookCommentsController < ApplicationController
 
-
   def create
    @book = Book.find(params[:book_id])
    @book_comment = current_user.book_comments.new(book_comment_params)
@@ -11,15 +10,14 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-  @book = Book.find(params[:book_id])
- 	book_comment = @book.book_comments.find(params[:id])
-		book_comment.destroy
+   @book = Book.find(params[:book_id])
+   @book_comment = @book.book_comments.find(params[:id])
+   @book_comment.destroy
   end
 
   private
   def book_comment_params
-   params.require(:book_comment).permit(:comment, :book_id, :user_id)
+   params.require(:book_comment).permit(:comment)
   end
-
 
 end
